662810003c698cc9ea390651de60f9ac25fdd11b
dorohovgeorge <dorohovgeorge@gmail.com>
2024-10-30 00:53:33 +0300

    refactor: move koin modules and koin in separate functions and file

--- src/main/kotlin/coliver/Application.kt
+++ src/main/kotlin/coliver/Application.kt
@@ -1,78 +1,23 @@
 package coliver
 
-import coliver.dao.city.CityDAO
-import coliver.dao.city.CityDAOImpl
-import coliver.dao.home_owner.HomeOwnerDAO
-import coliver.dao.home_owner.HomeOwnerDAOImpl
-import coliver.dao.home_type.HomeTypeDAO
-import coliver.dao.home_type.HomeTypeDAOImpl
-import coliver.dao.metro.MetroDAO
-import coliver.dao.metro.MetroDAOImpl
-import coliver.dao.property.PropertyDAO
-import coliver.dao.property.PropertyDAOImpl
-import coliver.dao.users.UserDAO
-import coliver.dao.users.UserDAOImpl
 import coliver.plugins.configureDatabases
+import coliver.plugins.configureKoin
 import coliver.plugins.configureRouting
 import coliver.plugins.configureSerialization
-import coliver.services.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
-import org.koin.dsl.module
-import org.koin.ktor.plugin.Koin
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-val metroModule = module {
-    single<MetroDAO> { MetroDAOImpl() }
-    single { MetroService(get()) }
-}
-
-val cityModule = module {
-    single<CityDAO> { CityDAOImpl() }
-    single { CityService(get()) }
-}
-
-val homeTypeModule = module {
-    single<HomeTypeDAO> { HomeTypeDAOImpl() }
-    single { HomeTypeService(get()) }
-}
-
-val homeOwnerModule = module {
-    single<HomeOwnerDAO> { HomeOwnerDAOImpl() }
-    single { HomeOwnerService(get()) }
-}
-
-val propertyModule = module {
-    single<PropertyDAO> { PropertyDAOImpl() }
-    single { PropertyService(get()) }
-}
-
-val userModule = module {
-    single<UserDAO> { UserDAOImpl() }
-    single { UserService(get()) }
-}
-
 fun Application.module() {
-    install(Koin) {
-        modules(
-            listOf(
-                metroModule,
-                cityModule,
-                homeTypeModule,
-                homeOwnerModule,
-                propertyModule,
-                userModule
-            )
-        )
-    }
     install(CORS) {
         anyHost()
         allowHeader(HttpHeaders.ContentType)
     }
     configureSerialization()
     configureDatabases()
+    configureKoin()
     configureRouting()
 }
 
---
+++ src/main/kotlin/coliver/plugins/Koin.kt
@@ -0,0 +1,63 @@
+package coliver.plugins
+
+import coliver.dao.city.CityDAO
+import coliver.dao.city.CityDAOImpl
+import coliver.dao.home_owner.HomeOwnerDAO
+import coliver.dao.home_owner.HomeOwnerDAOImpl
+import coliver.dao.home_type.HomeTypeDAO
+import coliver.dao.home_type.HomeTypeDAOImpl
+import coliver.dao.metro.MetroDAO
+import coliver.dao.metro.MetroDAOImpl
+import coliver.dao.property.PropertyDAO
+import coliver.dao.property.PropertyDAOImpl
+import coliver.dao.users.UserDAO
+import coliver.dao.users.UserDAOImpl
+import coliver.services.*
+import io.ktor.server.application.*
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
+
+val metroModule = module {
+    single<MetroDAO> { MetroDAOImpl() }
+    single { MetroService(get()) }
+}
+
+val cityModule = module {
+    single<CityDAO> { CityDAOImpl() }
+    single { CityService(get()) }
+}
+
+val homeTypeModule = module {
+    single<HomeTypeDAO> { HomeTypeDAOImpl() }
+    single { HomeTypeService(get()) }
+}
+
+val homeOwnerModule = module {
+    single<HomeOwnerDAO> { HomeOwnerDAOImpl() }
+    single { HomeOwnerService(get()) }
+}
+
+val propertyModule = module {
+    single<PropertyDAO> { PropertyDAOImpl() }
+    single { PropertyService(get()) }
+}
+
+val userModule = module {
+    single<UserDAO> { UserDAOImpl() }
+    single { UserService(get()) }
+}
+
+fun Application.configureKoin() {
+    install(Koin) {
+        modules(
+            listOf(
+                metroModule,
+                cityModule,
+                homeTypeModule,
+                homeOwnerModule,
+                propertyModule,
+                userModule
+            )
+        )
+    }
+}
