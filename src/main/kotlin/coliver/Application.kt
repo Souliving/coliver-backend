1f2d4815554d617e4ec41b80a28f2e4db067c742
dorohovgeorge <dorohovgeorge@gmail.com>
2025-06-07 17:13:56 +0300

    style: move auth and cors in files

--- src/main/kotlin/coliver/Application.kt
+++ src/main/kotlin/coliver/Application.kt
@@ -1,71 +1,18 @@
 package coliver
 
 import coliver.plugins.*
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.response.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain
         .main(args)
 
 fun Application.module() {
-    install(CORS) {
-        anyHost()
-        allowHeader(HttpHeaders.ContentType)
-        allowHeader(HttpHeaders.Authorization)
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Patch)
-        allowMethod(HttpMethod.Delete)
-    }
-    val secret = environment.config.property("jwt.secret").getString()
-    val issuer = environment.config.property("jwt.issuer").getString()
-    val audience = environment.config.property("jwt.audience").getString()
-
-    install(Authentication) {
-        jwt("userAuth") {
-            verifier(
-                JWT
-                    .require(Algorithm.HMAC256(secret))
-                    .withAudience(audience)
-                    .withIssuer(issuer)
-                    .build(),
-            )
-            validate { credential ->
-                if (credential.payload.getClaim("email").asString() != "" &&
-//                    && credential.payload.getClaim("role").asString() == "USER"
-                    credential.payload.getClaim("refresh").isMissing
-                ) {
-                    JWTPrincipal(credential.payload)
-                } else {
-                    null
-                }
-            }
-            challenge { _, _ ->
-                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
-            }
-        }
-    }
     configureSerialization()
     configureDatabases()
     configureKoin()
     configureMonitoring()
     configureRouting()
+    configureAuthentication()
+    configureCORS()
 }
-
-/*
-* val client = HttpClient(Apache5) {
-    engine {
-    }
-    install(ContentNegotiation) {
-        json()
-    }
-}
-val metro: List<Metro> = client.get("https://api.coliver.tech/api/v1/metro/").body()
-* */
---
+++ src/main/kotlin/coliver/plugins/Authentication.kt
@@ -0,0 +1,41 @@
+package coliver.plugins
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.auth.Authentication
+import io.ktor.server.auth.jwt.JWTPrincipal
+import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.response.respond
+
+fun Application.configureAuthentication() {
+    val secret = environment.config.property("jwt.secret").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    install(Authentication) {
+        jwt("userAuth") {
+            verifier(
+                JWT
+                    .require(Algorithm.HMAC256(secret))
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .build(),
+            )
+            validate { credential ->
+                if (credential.payload.getClaim("email").asString() != "" &&
+//                    && credential.payload.getClaim("role").asString() == "USER"
+                    credential.payload.getClaim("refresh").isMissing
+                ) {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+            challenge { _, _ ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+            }
+        }
+    }
+}
